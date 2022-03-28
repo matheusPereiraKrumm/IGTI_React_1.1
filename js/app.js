9d3bf84e762c4ff50dc5311604233ed132fa9e96
Matheus Pereira <matheusadrianopereira@gmail.com>
2022-03-27 21:34:02 -0300

    first step

--- js/app.js
+++ js/app.js
@@ -1,16 +1,54 @@
+
+let catalog = document.getElementsByClassName("catalog")[0];
+let filter_name = document.getElementById("filter-name");
+let filter_brand = document.getElementById("filter-brand");
+let filter_type = document.getElementById("filter-type");
+let filter_order = document.getElementById("sort-type");
+let lista = undefined;
+
+fetch("http://makeup-api.herokuapp.com/api/v1/products.json")
+  .then((response) => response.json().then(printProducts))
+  .catch(error => catalog.innerHTML = "Erro ao carregar informações");
+
+
+function printProducts(productsFromApi) {
+  lista = productsFromApi;
+  imprimir();
+}
+
+function imprimir() {
+  if (lista) {
+    listaFiltrada = lista;
+    catalog.innerHTML = "";
+    if (filter_name?.value != ''){
+      listaFiltrada = lista.filter(prd => prd.name.includes(filter_name.value));
+    }
+    for (let product of listaFiltrada) {
+      catalog.appendChild(productItem(product))
+    }
+  }
+}
+
 //EXEMPLO DO CÓDIGO PARA UM PRODUTO
 function productItem(product) {
-  const item = `<div class="product" data-name="NYX Mosaic Powder Blush Paradise" data-brand="nyx" data-type="bronzer" tabindex="508">
+  var div = document.createElement('div');
+  div.classList.add("product")
+  div.dataset.name = product.name;
+  div.dataset.brand = product.brand;
+  div.dataset.type = product.product_type;
+  div.tabIndex = product.id;
+  div.innerHTML = `
   <figure class="product-figure">
-    <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/deedb7bd74bda43f062a09aab2ee1ec8_ra,w158,h184_pa,w158,h184.png" width="215" height="215" alt="NYX Mosaic Powder Blush Paradise" onerror="javascript:this.src='img/unavailable.png'">
+    <img src="http:${product.api_featured_image}" width="215" height="215" alt="${product.name}" onerror="javascript:this.src='img/unavailable.png'">
   </figure>
   <section class="product-description">
-    <h1 class="product-name">NYX Mosaic Powder Blush Paradise</h1>
-    <div class="product-brands"><span class="product-brand background-brand">Nyx</span>
-<span class="product-brand background-price">R$ 57.70</span></div>
+    <h1 class="product-name">${product.name}</h1>
+    <div class="product-brands"><span class="product-brand background-brand">${product.brand}</span>
+<span class="product-brand background-price">R$ ${(parseFloat(product.price) * 5.5).toFixed(2)}</span></div>
   </section>
-  // CARREGAR OS DETALHES
-</div>`;
+  ${loadDetails(product)}
+  `;
+  return div;
 }
 
 //EXEMPLO DO CÓDIGO PARA OS DETALHES DE UM PRODUTO
@@ -18,27 +56,28 @@ function loadDetails(product) {
   let details = `<section class="product-details"><div class="details-row">
         <div>Brand</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">nyx</div>
+          <div class="details-bar-bg" style="width= 250">${product.brand}</div>
         </div>
       </div><div class="details-row">
         <div>Price</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">10.49</div>
+          <div class="details-bar-bg" style="width= 250">R$ ${(parseFloat(product.price) * 5.5).toFixed(2)}</div>
         </div>
       </div><div class="details-row">
         <div>Rating</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">5</div>
+          <div class="details-bar-bg" style="width= 250">${product.rating ?? "No Information"}</div>
         </div>
       </div><div class="details-row">
         <div>Category</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250"></div>
+          <div class="details-bar-bg" style="width= 250">${product.category}</div>
         </div>
       </div><div class="details-row">
         <div>Product_type</div>
         <div class="details-bar">
-          <div class="details-bar-bg" style="width= 250">bronzer</div>
+          <div class="details-bar-bg" style="width= 250">${product.product_type}</div>
         </div>
       </div></section>`;
+  return details;
 }
